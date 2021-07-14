1e9fe749874154f9ed18453510e32eacedbfd44d
Varun Venkatesh <varunv153@gmail.com>
2021-07-15 00:36:43 +0530

    Homepage slight beauty

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link } from "react-router-dom";
-import {Button,Jumbotron,Row,Col, Image } from 'react-bootstrap';
+import {Jumbotron,Row,Col, Image } from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {FaChevronRight} from 'react-icons/fa';
 
 export class HomePage extends React.Component 
 {
@@ -9,13 +11,18 @@ export class HomePage extends React.Component
 		return(
 			<Row>
 				<Col>
-					<Jumbotron className="bg-white">
-						<h1>Take a Selfie,</h1>
-						<h4>Make countless Stickers and GIFs</h4>
+					<Jumbotron className="bg-white" style={{fontFamily: "Roboto,sans-serif"}}>
+						<h1 style={{color: "rgb(65, 65, 65)",fontSize: "42px", lineHeight: 1.1, fontWeight: 700}}>Take a Selfie,</h1>
+						<h1 style={{fontSize: "28px", fontWeight: 400}}>Make countless Stickers and GIFs</h1>
 						<br/>
 						<br/>
 						<Link to="/createBobble" className="mt-4">
-							<Button size="lg" variant="outline-light" style={{backgroundColor: "#FF9801"}} className="shadow">Create your bobble > </Button>
+							<p className="d-flex flex-row" style={{fontSize: "1rem", borderRadius: "12px",textDecoration: "none", width: "210px",padding: "15px 20px",backgroundColor: "rgb(255, 152, 0)",color: "white",boxShadow: "rgba(68, 68, 68, 0.4) 5px 5px 5px"}}>
+								Create your bobble	
+								<span className="ml-3">
+									<FaChevronRight />
+								</span>
+							</p>
 						</Link>
 					</Jumbotron>
 				</Col>
